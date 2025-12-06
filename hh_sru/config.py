ec5636f30e3834a9b63f45e4d232b52fd3850aeb
gmanka <gmankab@gmail.com>
2025-12-07 02:02:08 +0500

    [+] add history.txt to the config

--- hh_sru/config.py
+++ hh_sru/config.py
@@ -8,6 +8,10 @@ import os
 engine_to_use: Literal['chromium'] | Literal['firefox'] = 'chromium'
 # see readme.md for explanation why defaults to chromium
 
+os.environ['SE_AVOID_BROWSER_DOWNLOAD'] = 'true'
+# prevents automatic downloading of propretary chromium binary
+# instead please install open-source chromium builds via package manager
+
 xdg_state_home = Path(
     os.getenv(
         'XDG_STATE_HOME',
@@ -17,9 +21,8 @@ xdg_state_home = Path(
 hh_state_dir = xdg_state_home / 'hh_sru'
 chromium_dir = hh_state_dir / 'chromium'
 firefox_dir = hh_state_dir / 'firefox'
+history_path = hh_state_dir / 'history.txt'
+history_list: list[int] = []
+
 driver: selenium.webdriver.remote.webdriver.WebDriver
 driver_wait: selenium.webdriver.support.ui.WebDriverWait
-
-os.environ['SE_AVOID_BROWSER_DOWNLOAD'] = 'true'
-# prevents automatic downloading of propretary chromium binary
-# instead please install open-source chromium builds via package manager
